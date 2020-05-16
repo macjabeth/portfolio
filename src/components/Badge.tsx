15d2a1008b326195117dbc1b4cca037660171bff
macjabeth <macbethjonathan@gmail.com>
2020-05-16 19:39:33 -0400

    add site favicorn

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const Badge = (props) => (
-  <li className="mt-2 inline-flex justify-center items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-orange-100 text-orange-800">
+const Badge: React.FC = (props) => (
+  <li
+    className="mt-2 inline-flex justify-center items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-orange-100 text-orange-800">
     {props.children}
   </li>
 );
