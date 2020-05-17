0722fe6f06164605e9c1a4a0d751cc80b4a36bf5
macjabeth <macbethjonathan@gmail.com>
2020-05-17 19:00:13 -0400

    add skeleton loaders for repo content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import { IGithubUser } from '../types/github';
 
 interface IHeaderProps {
@@ -12,21 +13,27 @@ const Header: React.FC<IHeaderProps> = ({ repo }) => (
            className="h-28 w-28 border-4 border-teal-100 rounded-full"
            alt="Jonathan's Photo" />
     ) : (
-      <span
-        className="inline-flex items-center justify-center h-28 w-28 rounded-full bg-teal-400">
-          <span
-            className="text-xl font-medium leading-none text-white">JP</span>
-        </span>
+      <SkeletonTheme color="#d5f5f6" highlightColor="#afecef">
+        <Skeleton circle={true} height={110} width={110} />
+      </SkeletonTheme>
     )}
     <h1 className="font-black mt-4">Jonathan Picazo</h1>
-    <a href={repo?.html_url || '#'} target="_blank" rel="noopener"
-       className="mt-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-indigo-100 text-indigo-800">
-      <svg className="-ml-0.5 mr-1.5 h-2 w-2 text-indigo-400"
-           fill="currentColor" viewBox="0 0 8 8">
-        <circle cx="4" cy="4" r="3" />
-      </svg>
-      {repo?.public_repos ?? '?'} repos
-    </a>
+    {repo?.avatar_url ? (
+      <a href={repo?.html_url || '#'} target="_blank" rel="noopener"
+         className="mt-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-indigo-100 text-indigo-800">
+        <svg className="-ml-0.5 mr-1.5 h-2 w-2 text-indigo-400"
+             fill="currentColor" viewBox="0 0 8 8">
+          <circle cx="4" cy="4" r="3" />
+        </svg>
+        {repo?.public_repos ?? '?'} repos
+      </a>
+    ) : (
+      <div id="loader">
+        <SkeletonTheme color="#e5edff" highlightColor="#cddbfe">
+          <Skeleton width={89.28} height={20} />
+        </SkeletonTheme>
+      </div>
+    )}
   </header>
 );
 
